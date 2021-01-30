e97fa1bb6706eed768f02f817131d35ad9c59f15
dwm <dwmcrobb@me.com>
2021-01-30 17:23:20 -0500

    Clean up a bit and add bulk interfaces.

--- classes/include/DwmIpv4PrefixMap.hh
+++ classes/include/DwmIpv4PrefixMap.hh
@@ -42,13 +42,15 @@
 #ifndef _DWMIPV4PREFIXMAP_HH_
 #define _DWMIPV4PREFIXMAP_HH_
 
+#include <cassert>
 #include <map>
+#include <mutex>
+#include <shared_mutex>
+#include <unordered_map>
 #include <vector>
 
-#include "DwmIpv4AddrMap.hh"
+// #include "DwmIpv4AddrMap.hh"
 #include "DwmIpv4Prefix.hh"
-#include "DwmReadLockedReference.hh"
-#include "DwmWriteLockedReference.hh"
 
 namespace Dwm {
 
@@ -81,7 +83,7 @@ namespace Dwm {
     Ipv4PrefixMap()
         : _mtx(), _map(), _lengthCounters()
     {
-      _map.max_load_factor(.25);
+      _map.max_load_factor(.15);
     }
     
     //------------------------------------------------------------------------
@@ -90,25 +92,49 @@ namespace Dwm {
     void Add(const Ipv4Prefix & pfx, const T & value)
     {
       std::unique_lock  lck(_mtx);
-      if (_map.insert_or_assign(pfx, value).second) {
-        _lengthCounters[pfx.MaskLength()]++;
-      }
-      return;
+      return Add(lck, pfx, value);
     }
 
     //------------------------------------------------------------------------
     //!  
     //------------------------------------------------------------------------
+    void Add(std::unique_lock<std::shared_mutex> & lck,
+             const Ipv4Prefix & pfx, const T & value)
+    {
+      assert((lck.mutex() == &_mtx) && lck.owns_lock());
+      if (_map.insert_or_assign(pfx, value).second) {
+        _lengthCounters[pfx.MaskLength()]++;
+      }
+      return;
+    }
+    
+    //------------------------------------------------------------------------
+    //!  
+    //------------------------------------------------------------------------
     bool Find(const Ipv4Prefix & pfx, T & value) const
     {
-      bool  rc = false;
       std::shared_lock  lck(_mtx);
-      auto  it = _map.find(pfx);
-      if (it != _map.end()) {
-        value = it->second;
-        rc = true;
-      }
-      return rc;
+      return Find(lck, pfx, value);
+    }
+
+    //------------------------------------------------------------------------
+    //!  
+    //------------------------------------------------------------------------
+    bool Find(std::shared_lock<std::shared_mutex> & lck,
+              const Ipv4Prefix & pfx, T & value) const
+    {
+      assert((lck.mutex() == &_mtx) && lck.owns_lock());
+      return FindNoLock(pfx, value);
+    }
+
+    //------------------------------------------------------------------------
+    //!  
+    //------------------------------------------------------------------------
+    bool Find(std::unique_lock<std::shared_mutex> & lck,
+              const Ipv4Prefix & pfx, T & value)
+    {
+      assert((lck.mutex() == &_mtx) && lck.owns_lock());
+      return FindNoLock(pfx, value);
     }
 
     //------------------------------------------------------------------------
@@ -117,21 +143,30 @@ namespace Dwm {
     bool FindLongest(const Ipv4Address & addr,
                      std::pair<Ipv4Prefix,T> & value) const
     {
-      bool  rc = false;
-      Ipv4Prefix  pfx(addr, 32);
       std::shared_lock  lck(_mtx);
-      for (auto lit = _lengthCounters.rbegin();
-           lit != _lengthCounters.rend(); ++lit) {
-        pfx.MaskLength(lit->first);
-        auto  it = _map.find(pfx);
-        if (it != _map.end()) {
-          value.first = pfx;
-          value.second = it->second;
-          rc = true;
-          break;
-        }
-      }
-      return rc;
+      return FindLongest(lck, addr, value);
+    }
+    
+    //------------------------------------------------------------------------
+    //!  
+    //------------------------------------------------------------------------
+    bool FindLongest(std::shared_lock<std::shared_mutex> & lck,
+                     const Ipv4Address & addr,
+                     std::pair<Ipv4Prefix,T> & value) const
+    {
+      assert((lck.mutex() == &_mtx) && lck.owns_lock());
+      return FindLongestNoLock(addr, value);
+    }
+
+    //------------------------------------------------------------------------
+    //!  
+    //------------------------------------------------------------------------
+    bool FindLongest(std::unique_lock<std::shared_mutex> & lck,
+                     const Ipv4Address & addr,
+                     std::pair<Ipv4Prefix,T> & value)
+    {
+      assert((lck.mutex() == &_mtx) && lck.owns_lock());
+      return FindLongestNoLock(addr, value);
     }
 
     //------------------------------------------------------------------------
@@ -140,29 +175,48 @@ namespace Dwm {
     bool FindMatches(const Ipv4Address & addr,
                      std::vector<std::pair<Ipv4Prefix,T>> & values) const
     {
-      values.clear();
-      std::pair<Ipv4Prefix,T>  value;
       std::shared_lock  lck(_mtx);
-      for (auto lit = _lengthCounters.rbegin();
-           lit != _lengthCounters.rend(); ++lit) {
-        Ipv4Prefix  pfx(addr, lit->first);
-        auto  it = _map.find(pfx);
-        if (it != _map.end()) {
-          value.first = pfx;
-          value.second = it->second;
-          values.push_back(value);
-        }
-      }
-      return (! values.empty());
+      return FindMatches(lck, addr, values);
     }
     
     //------------------------------------------------------------------------
     //!  
     //------------------------------------------------------------------------
+    bool FindMatches(std::shared_lock<std::shared_mutex> & lck,
+                     const Ipv4Address & addr,
+                     std::vector<std::pair<Ipv4Prefix,T>> & values) const
+    {
+      assert((lck.mutex() == &_mtx) && lck.owns_lock());
+      return FindMatchesNoLock(addr, values);
+    }
+
+    //------------------------------------------------------------------------
+    //!  
+    //------------------------------------------------------------------------
+    bool FindMatches(std::unique_lock<std::shared_mutex> & lck,
+                     const Ipv4Address & addr,
+                     std::vector<std::pair<Ipv4Prefix,T>> & values)
+    {
+      assert((lck.mutex() == &_mtx) && lck.owns_lock());
+      return FindMatchesNoLock(addr, values);
+    }
+
+    //------------------------------------------------------------------------
+    //!  
+    //------------------------------------------------------------------------
     bool Remove(const Ipv4Prefix & pfx)
     {
-      bool  rc = false;
       std::unique_lock  lck(_mtx);
+      return Remove(lck, pfx);
+    }
+    
+    //------------------------------------------------------------------------
+    //!  
+    //------------------------------------------------------------------------
+    bool Remove(std::unique_lock<std::shared_mutex> & lck,
+                const Ipv4Prefix & pfx)
+    {
+      bool  rc = false;
       auto  it = _map.find(pfx);
       if (it != _map.end()) {
         _map.erase(it);
@@ -179,24 +233,90 @@ namespace Dwm {
     //------------------------------------------------------------------------
     //!  
     //------------------------------------------------------------------------
-    ReadLockedReference<Ipv4PrefixMap<T>,MapType> ReadLockedRef() const
+    void Rehash(typename MapType::size_type count)
     {
-      return ReadLockedReference<Ipv4PrefixMap<T>,MapType>(_mtx, _map);
+      _map.rehash(count);
     }
 
     //------------------------------------------------------------------------
     //!  
     //------------------------------------------------------------------------
-    WriteLockedReference<Ipv4PrefixMap<T>,MapType> WriteLockedRef()
+    std::shared_lock<std::shared_mutex> SharedLock() const
     {
-      return WriteLockedReference<Ipv4PrefixMap<T>,MapType>(_mtx, _map);
+      return std::shared_lock(_mtx);
+    }
+
+    //------------------------------------------------------------------------
+    //!  
+    //------------------------------------------------------------------------
+    std::unique_lock<std::shared_mutex> UniqueLock()
+    {
+      return std::unique_lock(_mtx);
     }
     
-
   private:
     mutable std::shared_mutex    _mtx;
     MapType                      _map;
     std::map<uint8_t,uint64_t>   _lengthCounters;
+
+    //------------------------------------------------------------------------
+    //!  
+    //------------------------------------------------------------------------
+    bool FindNoLock(const Ipv4Prefix & pfx, T & value) const
+    {
+      bool  rc = false;
+      auto  it = _map.find(pfx);
+      if (it != _map.end()) {
+        value = it->second;
+        rc = true;
+      }
+      return rc;
+    }
+
+    //------------------------------------------------------------------------
+    //!  
+    //------------------------------------------------------------------------
+    bool FindLongestNoLock(const Ipv4Address & addr,
+                           std::pair<Ipv4Prefix,T> & value) const
+    {
+      bool  rc = false;
+      Ipv4Prefix  pfx(addr, 32);
+      for (auto lit = _lengthCounters.rbegin();
+           lit != _lengthCounters.rend(); ++lit) {
+        pfx.MaskLength(lit->first);
+        auto  it = _map.find(pfx);
+        if (it != _map.end()) {
+          value.first = pfx;
+          value.second = it->second;
+          rc = true;
+          break;
+        }
+      }
+      return rc;
+    }
+    
+    //------------------------------------------------------------------------
+    //!  
+    //------------------------------------------------------------------------
+    bool
+    FindMatchesNoLock(const Ipv4Address & addr,
+                      std::vector<std::pair<Ipv4Prefix,T>> & values) const
+    {
+      values.clear();
+      std::pair<Ipv4Prefix,T>  value;
+      for (auto lit = _lengthCounters.rbegin();
+           lit != _lengthCounters.rend(); ++lit) {
+        Ipv4Prefix  pfx(addr, lit->first);
+        auto  it = _map.find(pfx);
+        if (it != _map.end()) {
+          value.first = pfx;
+          value.second = it->second;
+          values.push_back(value);
+        }
+      }
+      return (! values.empty());
+    }
+
   };
   
 }  // namespace Dwm
