996b7cd38eaedea701b70a03f61f3478da5da928
dwm <dwmcrobb@me.com>
2024-07-20 13:52:05 -0400

    Add BootTime() and Uptime() and unit tests for them.

---
+++ classes/src/DwmUptime.cc
@@ -0,0 +1,85 @@
+//===========================================================================
+// @(#) $DwmPath$
+//===========================================================================
+//  Copyright (c) Daniel W. McRobb 2024
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions
+//  are met:
+//
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//  3. The names of the authors and copyright holders may not be used to
+//     endorse or promote products derived from this software without
+//     specific prior written permission.
+//
+//  IN NO EVENT SHALL DANIEL W. MCROBB BE LIABLE TO ANY PARTY FOR
+//  DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES,
+//  INCLUDING LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE,
+//  EVEN IF DANIEL W. MCROBB HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH
+//  DAMAGE.
+//
+//  THE SOFTWARE PROVIDED HEREIN IS ON AN "AS IS" BASIS, AND
+//  DANIEL W. MCROBB HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT,
+//  UPDATES, ENHANCEMENTS, OR MODIFICATIONS. DANIEL W. MCROBB MAKES NO
+//  REPRESENTATIONS AND EXTENDS NO WARRANTIES OF ANY KIND, EITHER
+//  IMPLIED OR EXPRESS, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE,
+//  OR THAT THE USE OF THIS SOFTWARE WILL NOT INFRINGE ANY PATENT,
+//  TRADEMARK OR OTHER RIGHTS.
+//===========================================================================
+
+//---------------------------------------------------------------------------
+//!  \file DwmUptime.cc
+//!  \author Daniel W. McRobb
+//!  \brief NOT YET DOCUMENTED
+//---------------------------------------------------------------------------
+
+#if (defined(__FreeBSD__) || defined(__APPLE__))
+  extern "C" {
+    #include <sys/types.h>
+    #include <sys/sysctl.h>
+  }
+#elif (defined(__linux__))
+  #include <fstream>  
+#endif
+
+#include "DwmUptime.hh"
+
+namespace Dwm {
+
+  //--------------------------------------------------------------------------
+  //!  
+  //--------------------------------------------------------------------------
+  time_t Uptime()
+  {
+    time_t  rc = 0;
+#if (defined(__FreeBSD__) || defined(__APPLE__))
+    struct timeval  tv = { .tv_sec = 0, .tv_usec = 0 };
+    int mib[2];
+    size_t len = sizeof(tv);
+    mib[0] = CTL_KERN;
+    mib[1] = KERN_BOOTTIME;
+    if (sysctl(mib, 2, &tv, &len, NULL, 0) == 0) {
+      time_t  now = time((time_t *)0);
+      if (now >= tv.tv_sec) {
+        rc = now - tv.tv_sec;
+      }
+    }
+#elif (defined(__linux__))
+    std::ifstream  is("/proc/uptime");
+    if (is) {
+      double d;
+      is >> d;
+      is.close();
+      rc = d;
+    }
+#endif
+    return rc;
+  }
+  
+}  // namespace Dwm
