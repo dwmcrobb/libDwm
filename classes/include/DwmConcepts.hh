808e4f5b55f26d3f074443bbc31de83bf68a6753
Daniel W. McRobb <dwm@thrip.mcplex.net>
2025-09-17 00:10:06 -0400

    Add some concepts I will be using to refactor some of the I/O classesto accomodate C++26 reflection.

---
+++ classes/include/DwmConcepts.hh
@@ -0,0 +1,142 @@
+//===========================================================================
+// @(#) $DwmPath$
+//===========================================================================
+//  Copyright (c) Daniel W. McRobb 2025
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions
+//  are met:
+//
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//  3. The names of the authors and copyright holders may not be used to
+//     endorse or promote products derived from this software without
+//     specific prior written permission.
+//
+//  IN NO EVENT SHALL DANIEL W. MCROBB BE LIABLE TO ANY PARTY FOR
+//  DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES,
+//  INCLUDING LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE,
+//  EVEN IF DANIEL W. MCROBB HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH
+//  DAMAGE.
+//
+//  THE SOFTWARE PROVIDED HEREIN IS ON AN "AS IS" BASIS, AND
+//  DANIEL W. MCROBB HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT,
+//  UPDATES, ENHANCEMENTS, OR MODIFICATIONS. DANIEL W. MCROBB MAKES NO
+//  REPRESENTATIONS AND EXTENDS NO WARRANTIES OF ANY KIND, EITHER
+//  IMPLIED OR EXPRESS, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE,
+//  OR THAT THE USE OF THIS SOFTWARE WILL NOT INFRINGE ANY PATENT,
+//  TRADEMARK OR OTHER RIGHTS.
+//===========================================================================
+
+//---------------------------------------------------------------------------
+//!  \file DwmConcepts.hh
+//!  \author Daniel W. McRobb
+//!  \brief NOT YET DOCUMENTED
+//---------------------------------------------------------------------------
+
+#include <array>
+#include <deque>
+#include <list>
+#include <map>
+#include <mutex>
+#include <set>
+#include <tuple>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <variant>
+#include <vector>
+
+#if defined(__cpp_impl_reflection)
+#  if defined(__cpp_expansion_statements)
+#    if __has_include(<meta>)
+#      include <meta>
+#      if defined(__cpp_lib_define_static)
+#        define DWMSTREAMIO_CAN_USE_REFLECTION 1
+#      endif
+#    endif
+#  endif
+#endif
+
+namespace Dwm {
+
+  namespace Concepts {
+
+#if defined(DWMSTREAMIO_CAN_USE_REFLECTION)
+    //  With reflection, it's easy to define a concept that can test if
+    //  a given type matches a given template, since we have template_of().
+    //  The macro here is just to reduce the amount of boilerplate code
+    //  to maintain in order to have a list of concepts to match specific
+    //  templates from the std namespace.
+    //  dwm TODO: I probably don't need the has_template_arguments() test.
+    #define MAKE_IS_STD_CONT_CONCEPT(x)                                    \
+      template <class T> concept is_std_##x =                              \
+        has_template_arguments(^^T) && template_of(^^T) == ^^std::x
+
+    //  With reflection, std::array can be matched in the same manner as
+    //  other containers.
+    MAKE_IS_STD_CONT_CONCEPT(array);
+#else
+    //  Without reflection, things are a little bit more awkward.  There
+    //  is probably a cleaner way to do what's here, I just haven't dug
+    //  deeply yet.  The main issue is that std::array's second template
+    //  parameter is a non-type parameter, while all the other containers'
+    //  template parameters are type parameters.  So I need a separate
+    //  set of traits for std::array.
+    template <class T>
+    inline constexpr bool is_std_array = false;
+    
+    template <class T, size_t _Size>
+    inline constexpr bool is_std_array<std::array<T, _Size>> = true;
+
+    template <class T, template <class...> class _Template>
+    inline constexpr bool is_std_container = false;
+    
+    template <template <class...> class _Tmpl, class... _Args>
+    inline constexpr bool is_std_container<_Tmpl<_Args...>, _Tmpl> = true;
+    
+    #define MAKE_IS_STD_CONT_CONCEPT(x) \
+    template <class T> concept is_std_##x = is_std_container<T,std::x>
+#endif
+
+    MAKE_IS_STD_CONT_CONCEPT(pair);
+    MAKE_IS_STD_CONT_CONCEPT(list);
+    MAKE_IS_STD_CONT_CONCEPT(deque);
+    MAKE_IS_STD_CONT_CONCEPT(map);
+    MAKE_IS_STD_CONT_CONCEPT(multimap);
+    MAKE_IS_STD_CONT_CONCEPT(set);
+    MAKE_IS_STD_CONT_CONCEPT(multiset);
+    MAKE_IS_STD_CONT_CONCEPT(tuple);
+    MAKE_IS_STD_CONT_CONCEPT(unordered_map);
+    MAKE_IS_STD_CONT_CONCEPT(unordered_multimap);
+    MAKE_IS_STD_CONT_CONCEPT(unordered_set);
+    MAKE_IS_STD_CONT_CONCEPT(unordered_multiset);
+    MAKE_IS_STD_CONT_CONCEPT(variant);
+    MAKE_IS_STD_CONT_CONCEPT(vector);
+    
+    template <class T> concept is_std_associative_container =
+      is_std_set<T>
+      or is_std_multiset<T>
+      or is_std_unordered_set<T>
+      or is_std_unordered_multiset<T>;
+    
+    template <typename T> concept is_std_pair_associative_container =
+      is_std_map<T>
+      or is_std_multimap<T>
+      or is_std_unordered_map<T>
+      or is_std_unordered_multimap<T>;
+    
+    template <typename T> concept is_std_sequence_container =
+      is_std_array<T>
+      or is_std_deque<T>
+      or is_std_list<T>
+      or is_std_vector<T>;
+    
+  }  // namespace Concepts
+  
+}  // namespace Dwm
---
+++ classes/tests/TestConcepts.cc
@@ -0,0 +1,144 @@
+//===========================================================================
+// @(#) $DwmPath$
+//===========================================================================
+//  Copyright (c) Daniel W. McRobb 2025
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms, with or without
+//  modification, are permitted provided that the following conditions
+//  are met:
+//
+//  1. Redistributions of source code must retain the above copyright
+//     notice, this list of conditions and the following disclaimer.
+//  2. Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//  3. The names of the authors and copyright holders may not be used to
+//     endorse or promote products derived from this software without
+//     specific prior written permission.
+//
+//  IN NO EVENT SHALL DANIEL W. MCROBB BE LIABLE TO ANY PARTY FOR
+//  DIRECT, INDIRECT, SPECIAL, INCIDENTAL, OR CONSEQUENTIAL DAMAGES,
+//  INCLUDING LOST PROFITS, ARISING OUT OF THE USE OF THIS SOFTWARE,
+//  EVEN IF DANIEL W. MCROBB HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH
+//  DAMAGE.
+//
+//  THE SOFTWARE PROVIDED HEREIN IS ON AN "AS IS" BASIS, AND
+//  DANIEL W. MCROBB HAS NO OBLIGATION TO PROVIDE MAINTENANCE, SUPPORT,
+//  UPDATES, ENHANCEMENTS, OR MODIFICATIONS. DANIEL W. MCROBB MAKES NO
+//  REPRESENTATIONS AND EXTENDS NO WARRANTIES OF ANY KIND, EITHER
+//  IMPLIED OR EXPRESS, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE,
+//  OR THAT THE USE OF THIS SOFTWARE WILL NOT INFRINGE ANY PATENT,
+//  TRADEMARK OR OTHER RIGHTS.
+//===========================================================================
+
+//---------------------------------------------------------------------------
+//!  \file TestConcepts.cc
+//!  \author Daniel W. McRobb
+//!  \brief NOT YET DOCUMENTED
+//---------------------------------------------------------------------------
+
+#include "DwmConcepts.hh"
+#include "DwmUnitAssert.hh"
+
+using namespace std;
+using namespace Dwm;
+
+//----------------------------------------------------------------------------
+//!  
+//----------------------------------------------------------------------------
+static void TestContainers()
+{
+  UnitAssert((Concepts::is_std_array<array<int,5>>));
+  UnitAssert((Concepts::is_std_array<array<string,5>>));
+  UnitAssert((Concepts::is_std_pair<pair<int,string>>));
+  UnitAssert((Concepts::is_std_pair<pair<string,int>>));
+  UnitAssert((Concepts::is_std_list<list<int>>));
+  UnitAssert((Concepts::is_std_list<list<string>>));
+  UnitAssert((Concepts::is_std_deque<deque<int>>));
+  UnitAssert((Concepts::is_std_deque<deque<string>>));
+  UnitAssert((Concepts::is_std_map<map<int,string>>));
+  UnitAssert((Concepts::is_std_map<map<string,int>>));
+  UnitAssert((Concepts::is_std_multimap<multimap<int,string>>));
+  UnitAssert((Concepts::is_std_multimap<multimap<string,int>>));
+  UnitAssert((Concepts::is_std_set<set<int>>));
+  UnitAssert((Concepts::is_std_set<set<string>>));
+  UnitAssert((Concepts::is_std_multiset<multiset<int>>));
+  UnitAssert((Concepts::is_std_multiset<multiset<string>>));
+  UnitAssert((Concepts::is_std_tuple<tuple<int,string,bool>>));
+  UnitAssert((Concepts::is_std_tuple<tuple<string,bool,int>>));
+  UnitAssert((Concepts::is_std_unordered_map<unordered_map<int,string>>));
+  UnitAssert((Concepts::is_std_unordered_map<unordered_map<string,int>>));
+  UnitAssert((Concepts::is_std_unordered_multimap<unordered_multimap<int,string>>));
+  UnitAssert((Concepts::is_std_unordered_multimap<unordered_multimap<string,int>>));
+  UnitAssert((Concepts::is_std_unordered_set<unordered_set<int,string>>));
+  UnitAssert((Concepts::is_std_unordered_set<unordered_set<string,int>>));
+  UnitAssert((Concepts::is_std_unordered_multiset<unordered_multiset<int,string>>));
+  UnitAssert((Concepts::is_std_unordered_multiset<unordered_multiset<string,int>>));
+  UnitAssert((Concepts::is_std_variant<variant<int,string,bool>>));
+  UnitAssert((Concepts::is_std_variant<variant<string,bool,int>>));
+  UnitAssert((Concepts::is_std_vector<vector<int>>));
+  UnitAssert((Concepts::is_std_vector<vector<string>>));
+
+  UnitAssert((Concepts::is_std_sequence_container<array<int,5>>));
+  UnitAssert((Concepts::is_std_sequence_container<deque<int>>));
+  UnitAssert((Concepts::is_std_sequence_container<list<int>>));
+  UnitAssert((Concepts::is_std_sequence_container<vector<int>>));
+  
+  UnitAssert((Concepts::is_std_associative_container<set<int>>));
+  UnitAssert((Concepts::is_std_associative_container<multiset<int>>));
+  UnitAssert((Concepts::is_std_associative_container<unordered_set<int>>));
+  UnitAssert((Concepts::is_std_associative_container<unordered_multiset<int>>));
+
+  UnitAssert((Concepts::is_std_pair_associative_container<map<string,int>>));
+  UnitAssert((Concepts::is_std_pair_associative_container<multimap<string,int>>));
+  UnitAssert((Concepts::is_std_pair_associative_container<unordered_map<string,int>>));
+  UnitAssert((Concepts::is_std_pair_associative_container<unordered_multimap<string,int>>));
+
+  UnitAssert((! Concepts::is_std_sequence_container<map<int,string>>));
+  UnitAssert((! Concepts::is_std_sequence_container<multimap<int,string>>));
+  UnitAssert((! Concepts::is_std_sequence_container<set<int>>));
+  UnitAssert((! Concepts::is_std_sequence_container<multiset<int>>));
+  UnitAssert((! Concepts::is_std_sequence_container<unordered_map<int,string>>));
+  UnitAssert((! Concepts::is_std_sequence_container<unordered_multimap<int,string>>));
+  UnitAssert((! Concepts::is_std_sequence_container<unordered_set<int>>));
+  UnitAssert((! Concepts::is_std_sequence_container<unordered_multiset<int>>));
+
+  UnitAssert((! Concepts::is_std_associative_container<array<int,5>>));
+  UnitAssert((! Concepts::is_std_associative_container<deque<int>>));
+  UnitAssert((! Concepts::is_std_associative_container<list<int>>));
+  UnitAssert((! Concepts::is_std_associative_container<vector<int>>));
+  UnitAssert((! Concepts::is_std_associative_container<map<int,string>>));
+  UnitAssert((! Concepts::is_std_associative_container<multimap<int,string>>));
+  UnitAssert((! Concepts::is_std_associative_container<unordered_map<int,string>>));
+  UnitAssert((! Concepts::is_std_associative_container<unordered_multimap<int,string>>));
+  
+  UnitAssert((! Concepts::is_std_pair_associative_container<array<int,5>>));
+  UnitAssert((! Concepts::is_std_pair_associative_container<deque<int>>));
+  UnitAssert((! Concepts::is_std_pair_associative_container<list<int>>));
+  UnitAssert((! Concepts::is_std_pair_associative_container<vector<int>>));
+  UnitAssert((! Concepts::is_std_pair_associative_container<set<int>>));
+  UnitAssert((! Concepts::is_std_pair_associative_container<multiset<int>>));
+  UnitAssert((! Concepts::is_std_pair_associative_container<unordered_set<int>>));
+  UnitAssert((! Concepts::is_std_pair_associative_container<unordered_multiset<int>>));
+  
+  return;
+}
+
+//----------------------------------------------------------------------------
+//!  
+//----------------------------------------------------------------------------
+int main(int argc, char *argv[])
+{
+  TestContainers();
+  
+  if (Assertions::Total().Failed()) {
+    Assertions::Print(cerr, true);
+    exit(1);
+  }
+  else {
+    cout << Assertions::Total() << " passed" << endl;
+    exit(0);
+  }
+}
