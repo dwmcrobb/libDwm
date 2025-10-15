25fe1f83e2aef1a3b17501ee7d185410996ca2bf
Daniel W. McRobb <dwm@thrip.mcplex.net>
2025-10-15 00:55:09 -0400

    Added NRead(int) and NWrite(int).

--- classes/include/DwmIpv6Address.hh
+++ classes/include/DwmIpv6Address.hh
@@ -188,6 +188,20 @@ namespace Dwm {
     ssize_t Write(int fd) const;
 
     //------------------------------------------------------------------------
+    //!  Reads from a file descriptor.  Returns the number of bytes read
+    //!  (16 on success).
+    //------------------------------------------------------------------------
+    inline ssize_t NRead(int fd)
+    { return Read(fd); }
+    
+    //------------------------------------------------------------------------
+    //!  Writes to a file descriptor.  Returns the number of bytes written
+    //!  (16 on success).
+    //------------------------------------------------------------------------
+    inline ssize_t NWrite(int fd) const
+    { return Write(fd); }
+
+    //------------------------------------------------------------------------
     //!  Reads from a FILE pointer.  Returns 1 on success, 0 on failure.
     //------------------------------------------------------------------------
     size_t Read(FILE * f);
