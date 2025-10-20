668b0050382b90b30ebf5b482a4b25cecdd02033
dwm <dwm@mcplex.net>
2025-10-20 15:26:39 -0400

    Documentation work

--- doc/manual/examples/Example06_UserDefinedType1_PostIOMain.cc
+++ doc/manual/examples/Example06_UserDefinedType1_PostIOMain.cc
@@ -1,11 +1,30 @@
 #include <sstream>
-#include "MyType.hh"
+#include "DwmStreamIO.hh"
 
-//----------------------------------------------------------------------------
+struct MyType {
+  uint32_t     id;
+  std::string  name;
+  std::string  category;
+
+#if ! defined(DWM_CAN_USE_REFLECTION)
+  std::istream & Read(std::istream & is)
+  { return Dwm::StreamIO::ReadV(is, id, name, category); }
+
+  std::ostream & Write(std::ostream & os) const
+  { return Dwm::StreamIO::WriteV(os, id, name, category); }
+#endif
+
+  bool operator <=> (const MyType &) const = default;  // default comparison
+};
+
 int main(int argc, char *argv[])
 {
   bool                 rc{false};
-  std::vector<MyType>  myvec1 { {1, "cat"}, {2, "dog" }, { 3, "fish" } };
+  std::vector<MyType>  myvec1 {
+    {1, "cat", "feline"},
+    {2, "dog", "canine" },
+    {3, "cow", "bovine" }
+  };
   std::stringstream    ss;
   if (Dwm::StreamIO::Write(ss, myvec1)) {
     std::vector<MyType>  myvec2;
--- doc/manual/examples/Example06_UserDefinedType1_PostIOMyType.hh
+++ doc/manual/examples/Example06_UserDefinedType1_PostIOMyType.hh
@@ -4,10 +4,12 @@ struct MyType {
   uint32_t     id;
   std::string  name;
   std::string  category;
-  
+
+#if ! defined(DWM_CAN_USE_REFLECTION)
   std::istream & Read(std::istream & is)
-  { return Dwm::StreamIO::ReadV(is, id, name); }
+  { return Dwm::StreamIO::ReadV(is, id, name, category); }
 
   std::ostream & Write(std::ostream & os) const
-  { return Dwm::StreamIO::WriteV(os, id, name); }
+  { return Dwm::StreamIO::WriteV(os, id, name, category); }
+#endif
 };
