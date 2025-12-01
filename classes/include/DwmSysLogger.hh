a17eeb1c7acf42fb4ca67fa3846e041f67c4ba4f
dwm <dwmcrobb@me.com>
2025-12-01 14:15:12 -0500

    Tweak detection of std::format to account for oddity on FreeBSD 13.3-STABLE

--- classes/include/DwmSysLogger.hh
+++ classes/include/DwmSysLogger.hh
@@ -53,15 +53,13 @@ extern "C" {
 #include <source_location>
 #include <string>
 
-#if 1
-
-#if defined(__cpp_lib_format)
-#  if (__cpp_lib_format >= 202110L)
-#    if __has_include(<format>)
-#       include <format>
+#if __has_include(<format>)
+#  include <format>
+// #  if defined(__cpp_lib_format)
+// #    if (__cpp_lib_format >= 202106L)
 #       define DWM_HAVE_STD_FORMAT
-#    endif
-#  endif
+// #    endif
+// #  endif
 #endif
 
 #ifndef DWM_HAVE_STD_FORMAT
@@ -69,20 +67,6 @@ extern "C" {
 #    include <fmt/format.h>
 #    define DWM_HAVE_LIBFMT
 #  endif
-#endif
-
-#else
-
-#include "DwmPortability.hh"
-
-#if (defined DWM_HAVE_STD_FORMAT)
-  #include <format>
-#else
-  #if (defined DWM_HAVE_LIBFMT)
-    #include <fmt/format.h>
-  #endif
-#endif
-
 #endif
 
 namespace Dwm {
