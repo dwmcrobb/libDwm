6ee3b75b433ae249e28674bce0d3ed83fa5d1c51
dwm <dwmcrobb@me.com>
2026-07-18 18:03:07 -0400

    Update performance number for i5-2405S

--- classes/include/DwmIpv6PrefixPatricia.hh
+++ classes/include/DwmIpv6PrefixPatricia.hh
@@ -50,7 +50,7 @@
 //!    - roughly 5.9 million lookups/second on a Mac Studio M1 Ultra.
 //!    - roughly 5.8 million lookups/second on an AMD Threadripper 3960X.
 //!    - roughly 4.3 million lookups/second on a Xeon E3-1270 V2 @ 3.50GHz.
-//!    - roughly ??? million lookups/second on an i5-2405S @ 2.50GHz.
+//!    - roughly 3.2 million lookups/second on an i5-2405S @ 2.50GHz.
 //!    - roughly 2.7 million lookups/second on a Xeon L5640 @ 2.27GHz.
 //!    - roughly 1.3 million lookups/second on a Raspberry Pi 4B.
 //---------------------------------------------------------------------------
