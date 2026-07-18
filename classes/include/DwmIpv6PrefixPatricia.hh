eaa0aeb4923e0735344bde2647f3d227768578fb
dwm <dwm@mcplex.net>
2026-07-18 17:39:17 -0400

    Update performance numbers for Raspberry Pi 4.

--- classes/include/DwmIpv6PrefixPatricia.hh
+++ classes/include/DwmIpv6PrefixPatricia.hh
@@ -45,14 +45,14 @@
 //!  that captures the shared common prefix.
 //!
 //!  6/11/2026 - some performance numbers using a ValueType of std::string,
-//!  using the 901,114 prefixes in ../tests/IPV4_prefixes.20210123:
+//!  using the 258,884 prefixes in ../tests/IPV6_prefixes.20260709:
 //!
 //!    - roughly 5.9 million lookups/second on a Mac Studio M1 Ultra.
 //!    - roughly 5.8 million lookups/second on an AMD Threadripper 3960X.
 //!    - roughly ??? million lookups/second on a Xeon E3-1270 V2 @ 3.50GHz.
 //!    - roughly ??? million lookups/second on an i5-2405S @ 2.50GHz.
 //!    - roughly ??? million lookups/second on a Xeon L5640 @ 2.27GHz.
-//!    - roughly ??? million lookups/second on a Raspberry Pi 4B.
+//!    - roughly 1.3 million lookups/second on a Raspberry Pi 4B.
 //---------------------------------------------------------------------------
 
 #ifndef _DWMIPV6PREFIXPATRICIA_HH_
